9a58a294948170af201729096793bb2daeb730ba
yuval23 <yuval.vardi@ncino.com>
2021-07-15 15:35:41 +0100

    adding JSForce as Direct connection

--- src/client/modules/core/app/app.js
+++ src/client/modules/core/app/app.js
@@ -1,5 +1,8 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import { getData } from '../../data/services/services';
+
+const LOGIN_INPUTS = ['username', 'instanceUrl', 'password', 'securityToken'];
+const DEFAULT_FLAGS = [{ name: 'json', type: 'boolean', disabled: true }, { name: 'username', type: 'text', disabled: false }];
 
 export default class App extends LightningElement {
     @api
@@ -10,44 +13,131 @@ export default class App extends LightningElement {
         this._pathName = `${value}`;
     }
 
-    message = 'Whoo hooo!!';
+    loading = false;
+    activeTab = 'intro';
     command = 'commands';
+
+    message = 'Whoo hooo!!';
+
     response = '';
+    loginFields = LOGIN_INPUTS;
 
     handleInputChange(event) {
         this.message = event.target.value;
     }
-
-    handleClick() {
-        // build query
-        const baseQuery = {
-            query: `{
-                hello(message:"${this.message}")
-            }`
-        };
-        this.fetchData(baseQuery);
+    handleActiveTab(event) {
+        this.activeTab = event.target.value;
     }
-    handleSfdxClick() {
-        const flags = `["--json"]`;
-        const sfdxQuery = {
+
+    // Handle Flag items - add/remove/view
+    @track _flags = DEFAULT_FLAGS;
+
+    handleAddFlag() {
+        this._flags.push({});
+    }
+    handleRemoveFlag(event) {
+        const flagIndex = parseInt(event.target.dataset.index);
+        if (flagIndex > 0) {
+            console.log(flagIndex);
+            this._flags.splice(flagIndex, 1);
+        }
+    }
+
+    get flags() {
+        return this._flags.map((item, index) => ({
+            ...item,
+            allowRemove: index > 0,
+            requireValue: item.type !== 'boolean',
+            index
+        }));
+    }
+
+    // Run callout button 
+    handleClick() {
+        // Build query
+        let graphQuery = { query: `{}` };
+        switch (this.activeTab) {
+            case 'intro':
+                graphQuery = { query: `{ hello(message:"${this.message}") }` };
+                break;
+            case 'sfdx':
+                graphQuery = this.generateSfdxCommand();
+                break;
+            case 'login':
+                graphQuery = this.generateLoginQuery();
+                break;
+        }
+        if (graphQuery) {
+            // Run Query
+            console.log('query: ' + graphQuery.query);
+            this.loading = true;
+            this.fetchData(graphQuery);
+        }
+    }
+
+    // Build an SFDX Command
+    generateSfdxCommand() {
+        let flags = ['--json'];
+        const flagInputs = this.template.querySelectorAll('.flag-input');
+        // get flag input values
+        if (flagInputs.length) {
+            flagInputs.forEach(input => {
+                this.flags.forEach(flag => {
+                    if (flag.name === input.name) {
+                        flags.push(`--${flag.name}=${input.value}`);
+                    }
+                })
+            });
+        }
+        return {
             query: `{
-                getSfdxCommands(
+                sfdxCommands(
                     command:"${this.command}",
-                    flags:${flags}
+                    flags:${JSON.stringify(flags)}
                     ){
                         total
                         commands{ id description } 
                     }
             }`
         };
-        this.fetchData(sfdxQuery);
     }
 
+    // This will pass the input values for the login using JS Force
+    generateLoginQuery() {
+        const orgInputs = this.checkValidity();
+        if (orgInputs) {
+            // format for graphQL Query
+            const orgCredentials = JSON.stringify(orgInputs).replace(/"([^"]+)":/g, '$1:');
+
+            // const sample = `{ 
+            //     username:"test-lm2urzytje72@example.com",
+            //     password:"K7_J!tUEA[A7O",
+            //     securityToken:"JULIUS_CCJR",
+            //     instanceUrl:"https://rapid-innovation-5550-dev-ed.cs18.my.salesforce.com" 
+            // }`;
+
+            return {
+                query: `{
+                    orgLogin( creds: ${orgCredentials} ){
+                        userId
+                        accessToken
+                        loggedInDate
+                        currentUser { display_name }
+                    }
+                }`
+            };
+        }
+        return;
+
+    }
+
+    // Call GraphQL Server
     async fetchData(query) {
         try {
             const response = await getData(query);
             if (response) {
                 console.log('SUCCESS ' + JSON.stringify(response));
+                this.loading = false;
                 this.response = JSON.stringify(response);
             }
 
@@ -56,7 +146,23 @@ export default class App extends LightningElement {
         }
     }
 
-    get pageTitle() {
-        return `Path : ${this.pathName}`;
+
+    // Check and inform form validy 
+    // Build object from input values
+    checkValidity() {
+        const inputs = this.template.querySelectorAll('.login-input');
+        let loginValuesObject = {};
+        const valid = [...inputs].reduce((validSoFar, inputField) => {
+            inputField.reportValidity();
+            return validSoFar && inputField.checkValidity();
+        }, true);
+        if (valid) {
+            inputs.forEach(el => {
+                if (el.value) {
+                    loginValuesObject[el.name] = el.value;
+                }
+            });
+        }
+        return valid ? loginValuesObject : false;
     }
 }
